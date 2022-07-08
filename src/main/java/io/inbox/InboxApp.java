aef62b5605c3c213350ea26f73dba9241a6db489
NY <nikhilyadav.nnn@gmail.com>
2022-07-08 22:37:29 +0530

    Initial Commit

---
+++ src/main/java/io/inbox/InboxApp.java
@@ -0,0 +1,46 @@
+package io.inbox;
+
+import java.nio.file.Path;
+
+import javax.annotation.PostConstruct;
+
+import io.inbox.email.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class InboxApp {
+
+	@Autowired
+	private EmailService emailService;
+
+	public static void main(String[] args) {
+		SpringApplication.run(InboxApp.class, args);
+	}
+
+	@Bean
+	public CqlSessionBuilderCustomizer sessionBuilderCustomizer(DataStaxAstraProperties astraProperties) {
+		Path bundle = astraProperties.getSecureConnectBundle().toPath();
+		return builder -> builder.withCloudSecureConnectBundle(bundle);
+	}
+
+	@PostConstruct
+	public void initializeData() {
+
+		for (int i = 0; i < 10; i++) {
+
+			emailService.sendEmail("koushikkothagal", "koushikkothagal", "Test " + i, "Body " + i);
+		}
+		
+
+	}
+
+}
